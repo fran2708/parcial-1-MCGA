1e7afa5f42b2098517f5830b41520a64e743257c
fran2708 <francopaladini2708@gmail.com>
2022-10-03 21:32:32 -0300

    Parcial1-05: fixed connection to mongoDB

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const productSchema = require("../models/Product");
+const productSchema = require("../models/Products");
 
 //create product
 router.post("/products", (req, res) => {
